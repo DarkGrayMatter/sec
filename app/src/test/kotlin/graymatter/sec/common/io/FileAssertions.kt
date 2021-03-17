fb24849f487c1e48096db16f71c90e6a9d29f8ab
Andries <69518948+andriesfc@users.noreply.github.com>
2021-03-17 22:17:51 +0200

    Made sure default for decrypting docs goes to stdout. (#37)

--- app/src/test/kotlin/graymatter/sec/common/io/FileAssertions.kt
+++ app/src/test/kotlin/graymatter/sec/common/io/FileAssertions.kt
@@ -19,18 +19,22 @@ fun assertFileHasContentOf(
     assertTrue(file.exists(), "File does not exists: $file")
     assertTrue(file.length() > 0, "File is empty: $file")
 
+    fun readAsProperties(): Properties {
+        return Properties().apply {
+            visitNodePathsOf(treeOf<ObjectNode>(format, file.readText())) {
+                if (node.isValueNode || node.isMissingNode) {
+                    val value = node.asText(null)
+                    val key = path.substring(1).replace('/', '.')
+                    setProperty(key, value)
+                }
+            }
+        }
+    }
+
     val props = assertDoesNotThrow({ "Error parsing file as ${format.name.toLowerCase()}: $file" }) {
         when (format) {
             DocumentFormat.JAVA_PROPERTIES -> Properties(file)
-            else -> Properties().apply {
-                visitNodePathsOf(treeOf<ObjectNode>(format, file.readText())) {
-                    if (node.isValueNode || node.isMissingNode) {
-                        val value = node.asText(null)
-                        val key = path.substring(1).replace('/', '.')
-                        setProperty(key, value)
-                    }
-                }
-            }
+            else -> readAsProperties()
         }
     }
 

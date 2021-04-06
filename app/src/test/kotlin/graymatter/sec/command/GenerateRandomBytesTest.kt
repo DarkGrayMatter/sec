5831cbf4f4e4bd5200a33ea0f26dc7cd2c3b5d37
andries <andriesfc@gmail.com>
2021-04-06 09:30:56 +0200

    Reworked unit test to make sure it is not the base test driver causing the unit test to fail (while actually working from the command line).

--- app/src/test/kotlin/graymatter/sec/command/GenerateRandomBytesTest.kt
+++ app/src/test/kotlin/graymatter/sec/command/GenerateRandomBytesTest.kt
@@ -1,19 +1,22 @@
 package graymatter.sec.command
 
+import com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut
+import graymatter.sec.App
 import graymatter.sec.common.crypto.BinaryEncoding
 import graymatter.sec.common.encodeBinary
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import kotlin.random.Random
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
-internal class GenerateRandomBytesTest : CommandTest<GenerateRandomBytes>() {
+internal class GenerateRandomBytesTest {
 
+    private var commandOutputLines: List<String>? = null
     private var seedHex: String? = null
     private var bytesOut: Int? = null
     private var repeat: Int? = null
-
-    override fun newCommand() = GenerateRandomBytes()
 
     @Test
     @Disabled("When running the cli args from the command line the desired results are achieved, but not in this unit test.")
@@ -26,20 +29,37 @@ internal class GenerateRandomBytesTest : CommandTest<GenerateRandomBytes>() {
         lateinit var expected: List<String>
         lateinit var actual: List<String>
 
-        repeat(2) {
+        repeat(2) { case ->
             whenRunningCommand()
-            when (it) {
-                0 -> expected = commandOutputLines!!
-                1 -> actual = commandOutputLines!!
+            val collected = commandOutputLines
+            assertNotNull(collected)
+            when (case) {
+                0 -> expected = collected
+                1 -> actual = collected
             }
         }
 
         assertEquals(expected, actual)
     }
 
-    override fun buildCommandLine() {
-        seedHex?.also { cliArgs("--seed", it, "--seed-encoding", "hex") }
-        bytesOut?.also { cliArgs("--bytes", it.toString()) }
-        repeat?.also { cliArgs("--repeat", it.toString()) }
+    private fun whenRunningCommand() {
+        commandOutputLines = null
+        val command = GenerateRandomBytes()
+        val commandLine = App.createCommandLine(command)
+        commandLine.parseArgs(* buildCommandLine())
+        commandOutputLines = tapSystemOut { assertDoesNotThrow { command.run() } }?.lines()
+    }
+
+    private fun buildCommandLine(): Array<String> {
+        val cliArgs = mutableListOf<String>()
+        seedHex?.also { cliArgs.add("--seed", it, "--seed-encoding", "hex") }
+        bytesOut?.also { cliArgs.add("--bytes", it.toString()) }
+        repeat?.also { cliArgs.add("--repeat", it.toString()) }
+        return cliArgs.toTypedArray()
+    }
+
+    private fun MutableList<String>.add(string: String, vararg more: String) {
+        add(string)
+        more.forEach(this::add)
     }
 }
